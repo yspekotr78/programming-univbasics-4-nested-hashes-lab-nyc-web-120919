33acf66b4976097f2def937cf7c5425e86b61c52
Yury Spektor <yspektor78@gmail.com>
2019-11-27 16:18:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,12 +82,14 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }
-#programmer_hash[:yukihiro_matsumoto][:languages "LISP","C"]
+     :known_for => "Ruby",
+     :languages => ["LISP", "C"]
+}
+
 return programmer_hash    
 end
+
+
 
 def changing_alan
 	programmer_hash = 
@@ -107,8 +109,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+programmer_hash[:alan_kay][:known_for] = "GUI"     
+return programmer_hash     
 end
 
 def adding_to_dennis
