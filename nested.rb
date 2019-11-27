264bba7fa24abca553f692671f3e392928ff99dd
Yury Spektor <yspektor78@gmail.com>
2019-11-27 16:22:23 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    return programmer_hash[:alan_kay][:known_for] 
 end
 
 def dennis_ritchies_language
@@ -52,9 +53,10 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -79,9 +81,15 @@ def adding_matz
           :languages => ["C"]
         }
      }
+programmer_hash[:yukihiro_matsumoto] = {
+     :known_for => "Ruby",
+     :languages => ["LISP", "C"]
+}
 
-    
+return programmer_hash    
 end
+
+
 
 def changing_alan
 	programmer_hash = 
@@ -101,9 +109,11 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+programmer_hash[:alan_kay][:known_for] = "GUI"     
+return programmer_hash     
 end
+
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -121,5 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages].push('Assembly')
+return programmer_hash
 end
