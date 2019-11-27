b0c970cca6b1ab2ae668fa70f4229ff578ad7136
Yury Spektor <yspektor78@gmail.com>
2019-11-27 16:07:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto][:known_for "Ruby"]
-programmer_hash[:yukihiro_matsumoto][:languages "LISP","C"]
+#programmer_hash[:yukihiro_matsumoto][:languages "LISP","C"]
 return programmer_hash    
 end
 
