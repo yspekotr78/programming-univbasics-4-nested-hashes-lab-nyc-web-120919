9d17356be71137dc8d8f42204a66e26c945f9886
Yury Spektor <yspektor78@gmail.com>
2019-11-27 16:01:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -53,7 +53,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
      return programmer_hash[:dennis_ritchie][:languages]
